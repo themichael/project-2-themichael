4d1524b9b52fb6175d28dd2c29eabad4e918b5c5
Michael Jaramillo <mikeormichael94@gmail.com>
2019-03-18 14:33:52 -0400

    uncommented test

--- src/test/java/com/revature/testing/UserServiceTest.java
+++ src/test/java/com/revature/testing/UserServiceTest.java
@@ -10,47 +10,47 @@ import com.revature.exception.LoginFailedException;
 import com.revature.model.User;
 import com.revature.service.UserService;
 
-// public class UserServiceTest {
-// 	private User testUser;
-// 	private User testLoginUser;
+public class UserServiceTest {
+	private User testUser;
+	private User testLoginUser;
 	
-//     @Before
-//     public void setUp() {
-//     	//Existing User in TEST DB
-//     	testUser = new User("palagnajr","");
-//     	//Invalid credentials
-//     	testLoginUser = new User("palagnajr","12345678");
-//     }
+    @Before
+    public void setUp() {
+    	//Existing User in TEST DB
+    	testUser = new User("palagnajr","");
+    	//Invalid credentials
+    	testLoginUser = new User("palagnajr","12345678");
+    }
     
-//     @After
-// 	public void tearDown() {
-// 		testUser = null;
-// 		testLoginUser = null;
-// 	}
+    @After
+	public void tearDown() {
+		testUser = null;
+		testLoginUser = null;
+	}
     
-//     /* Wrong login credentials, expected exception */
-//     @Test(expected = LoginFailedException.class)
-// 	public void loginTest() throws LoginFailedException {
-// 		UserService.getUserService().login(testLoginUser);
-// 	}
+    /* Wrong login credentials, expected exception */
+    @Test(expected = LoginFailedException.class)
+	public void loginTest() throws LoginFailedException {
+		UserService.getUserService().login(testLoginUser);
+	}
     
-//     /* Users existing in db, expected list with more than 0 users */
-//     @Test
-//     public void getAllUsersTest() {
-//     	assertTrue(UserService.getUserService().getAllEmployees().size() > 0);
-//     }
+    /* Users existing in db, expected list with more than 0 users */
+    @Test
+    public void getAllUsersTest() {
+    	assertTrue(UserService.getUserService().getAllEmployees().size() > 0);
+    }
     
-//     /* Username already exists in database, expected true */
-//     @Test
-// 	public void isUsernameTakenTest()  {
-// 		assertTrue(UserService.getUserService().isUsernameTaken(testUser.getUsername()));		
-// 	}
+    /* Username already exists in database, expected true */
+    @Test
+	public void isUsernameTakenTest()  {
+		assertTrue(UserService.getUserService().isUsernameTaken(testUser.getUsername()));		
+	}
    
-//     /* Username existing in db, expected same user as returned */
-//     @Test
-//     public void getUserInfoTest() {
-//     	User user = UserService.getUserService().getUserInfo(testUser.getUsername());
+    /* Username existing in db, expected same user as returned */
+    @Test
+    public void getUserInfoTest() {
+    	User user = UserService.getUserService().getUserInfo(testUser.getUsername());
     	
-//     	assertTrue(user.getUsername().equals(testUser.getUsername()));
-//     }
-// } 
+    	assertTrue(user.getUsername().equals(testUser.getUsername()));
+    }
+} 
